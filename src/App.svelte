b2468eeb099a386406aadb16c9c1b0347d998da1
Jinzhe Li <32780791+Jintze@users.noreply.github.com>
2020-08-09 22:33:25 -0600

    test exe

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
-<script>
+<!-- <script>
 	export let name;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	<h1>Hello TianSheng!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
@@ -14,15 +15,24 @@
 		max-width: 240px;
 		margin: 0 auto;
 	}
+
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
+
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
 	}
-</style>
+</style> -->
+
+<script>
+	import Todos from './Todos.svelte';
+	import WhiteNoise from './WhiteNoise.svelte'
+</script>
+<Todos />
+<WhiteNoise />
